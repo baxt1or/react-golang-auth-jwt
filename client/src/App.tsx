2b3db217e7ef15f9b8fd59b62d6c044288160704
baxt1or <230035@centralasian.com>
2024-06-16 16:24:24 +0500

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex flex-col">Hello</h1>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
